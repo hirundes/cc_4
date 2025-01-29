d3639a44525bfa4b4e31e48d35c7636606920d92
HIRUN <hirundesilva97@gmail.com>
2025-01-29 11:41:44 -0500

    Task 1 - Customer Discounts

--- cc_4.js
+++ cc_4.js
@@ -1,1 +1,17 @@
-Test 
+//Task 1: If Statements
+let purchaseAmount = 2500       //declaring purchaseAmount
+let finalAmount = purchaseAmount 
+if (purchaseAmount > 100) {
+    finalAmount = purchaseAmount * 0.75; 
+};      //if statement to apply discount if amount is greater than 100
+console.log("Task 1")
+console.log(`       Final amount after discount of 25%: $${finalAmount}`);
+
+//Task 2: For Loop
+let sales = [100, 200, 300, 400 ,500];
+let totalSales = 0; 
+for  (let i = 0; i < sales.length; i++) {
+    totalSales += sales[i];
+}
+console.log(`Total Sales:, $${totalSales}`);
+
